22a10f291d0bf2e2d8fc6e869dc8d674e149a746
Elliana May <me@mause.me>
2023-07-09 22:17:32 +0800

    fix: add materialized field

--- duckdb_ast/snapshots/snap_test_schema.py
+++ duckdb_ast/snapshots/snap_test_schema.py
@@ -294,13 +294,23 @@ src/include/duckdb/parser/expression/columnref_expression.hpp#L18''',
                     'title': 'Aliases',
                     'type': 'array'
                 },
+                'materialized': {
+                    'enum': [
+                        'CTE_MATERIALIZE_DEFAULT',
+                        'CTE_MATERIALIZE_ALWAYS',
+                        'CTE_MATERIALIZE_NEVER'
+                    ],
+                    'title': 'Materialized',
+                    'type': 'string'
+                },
                 'query': {
                     '$ref': '#/$defs/SelectStatement'
                 }
             },
             'required': [
                 'aliases',
-                'query'
+                'query',
+                'materialized'
             ],
             'title': 'CommonTableExpressionInfo',
             'type': 'object'
